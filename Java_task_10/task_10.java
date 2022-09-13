0a6d42895b9e133894e48b976285d4a93931adb1
Re_Gelu <morandva@yandex.ru>
2022-09-13 15:32:01 +0300

    Updated Task 10

--- Java_task_10/task_10.java
+++ Java_task_10/task_10.java
@@ -57,9 +57,6 @@ import java.util.Scanner;
 import java.util.*;
 public class task_10 {
 
-   /* 1. Треугольная последовательность. Дана монотонная последовательность, в которой каждое натуральное число k встречается 
-    ровно k раз: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,… По данному натуральному n выведите первые n членов этой последовательности. 
-    Попробуйте обойтись только одним циклом for. */
   static void task_1(int amount) {
     int k = 0;
     int j = 1;
@@ -77,9 +74,6 @@ public class task_10 {
     }
   }
 
-  /* 4. Заданная сумма цифр. Даны натуральные числа k и s. Определите, сколько существует k-значных натуральных чисел, 
-    сумма цифр которых равна d. Запись натурального числа не может начинаться с цифры 0. В этой задаче можно использовать 
-    цикл для перебора всех цифр, стоящих на какой-либо позиции. */
   static int task_4(int len, int sum, int k, int s) {
     if (len == k) {
       if (sum == s) {
@@ -96,26 +90,171 @@ public class task_10 {
     return res;
   }
 
-  /* 5. Сумма цифр числа. Дано натуральное число N. Вычислите сумму его цифр. При решении этой задачи нельзя использовать строки, списки, массивы (ну и циклы, разумеется). */
   static int task_5(int number) {
     return (number == 0) ? 0 : (number % 10) + task_5(number / 10);
   }
 
-  static boolean task_6(int n, int i) {
+  static String task_6(int n, int i) {
     // i- дополнительный параметр. При вызове должен быть равен 2
     if (n < 2) {
-      return false;
+      return "NO";
     }
     else if (n == 2) {
-      return true;
+      return "YES";
     }
     else if (n % i == 0) {
-      return false;
+      return "NO";
     }
     else if (i < n / 2) {
       return task_6(n, i + 1);
     } else {
-      return true;
+      return "YES";
+    }
+  }
+
+  static void task_7(int n, int k) {
+    // k- дополнительный параметр. При вызове должен быть равен 2
+    if (k > n / 2) {
+      System.out.print(n + " ");
+      return;
+    }
+    if (n % k == 0) {
+      System.out.print(k + " ");
+      task_7(n / k, k);
+    }
+    else {
+      task_7(n, ++k);
+    }
+  }
+  
+  static String task_8(String s) {
+    if (s.length() == 1) {
+      return "YES";
+    } else {
+      if (s.substring(0, 1).equals(s.substring(s.length() - 1, s.length()))) {
+        if (s.length() == 2) {
+          return "YES";
+        }
+        return task_8(s.substring(1, s.length() - 1));
+      } else {
+        return "NO";
+      }
+    }
+  }
+  
+  static int task_9(int a, int b) {
+    if (a > b + 1) {
+      return 0;
+    }
+    if (a == 0 || b == 0) {
+      return 1;
+    }
+    return task_9(a, b - 1) + task_9(a - 1, b - 1);
+  }
+
+  static int task__10(int n, int i) {
+    return (n == 0) ? i : task__10(n / 10, i * 10 + n % 10);
+  }
+
+  static int task_11() {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    // Базовый случай
+    if (n == 1) {
+      int m = in.nextInt();
+      // Базовый случай
+      if (m == 1) {
+        // Шаг рекурсии / рекурсивное условие
+        return task_11() + n + m;
+      } else {
+        int k = in.nextInt();
+        // Базовый случай
+        if (k == 1) {
+          // Шаг рекурсии / рекурсивное условие
+          return task_11() + n + m + k;
+        } else {
+          return n + m + k;
+        }
+      }
+    } else {
+      int m = in.nextInt();
+      // Базовый случай
+      if (m == 1) {
+        // Шаг рекурсии / рекурсивное условие
+        return task_11() + n + m;
+      } else {
+        return n + m;
+      }
+    }
+  }
+  
+  static void task_12() {
+    java.util.Scanner in = new java.util.Scanner(System.in);
+    int n = in.nextInt();
+    if (n > 0) {
+      if (n % 2 == 1) {
+        System.out.print(n + " ");
+        task_12();
+      } else {
+        task_12();
+      }
+    }
+  }
+
+  static void task_13() {
+    java.util.Scanner in = new java.util.Scanner(System.in);
+    int n = in.nextInt();
+    // Базовый случай
+    if (n > 0) {
+      int m = in.nextInt();
+      System.out.println(n);
+      // Базовый случай
+      if (m > 0) {
+        // Шаг рекурсии / рекурсивное условие
+        task_13();
+      }
+    }
+  }
+
+  static String task_14(int n) {
+    // Базовый случай
+    if (n < 10) {
+      return Integer.toString(n);
+    } // Шаг рекурсии / рекурсивное условие
+    else {
+      return task_14(n / 10) + " " + n % 10;
+    }
+  }
+
+  static void task_15(int max, int count) {
+    java.util.Scanner in = new java.util.Scanner(System.in);
+    int n = in.nextInt();
+    // Базовый случай
+    if (n > 0) {
+      // Шаг рекурсии / рекурсивное условие
+      if (n > max) {
+        task_15(n, 1);
+      } // Шаг рекурсии / рекурсивное условие
+      else if (n == max) {
+        task_15(max, ++count);
+      } // Шаг рекурсии / рекурсивное условие
+      else {
+        task_15(max, count);
+      }
+    } else {
+      System.out.println(count);
+    }
+  }
+
+  static int task_16() {
+    java.util.Scanner in = new java.util.Scanner(System.in);
+    int n = in.nextInt();
+    // Базовый случай
+    if (n == 0) {
+      return 0;
+    } // Шаг рекурсии / рекурсивное условие
+    else {
+      return Math.max(n, task_16());
     }
   }
 
@@ -171,7 +310,43 @@ public class task_10 {
     System.out.println("  Результат: " + task_6(N, 2));
 
     // 7)
-    System.out.println("7) Проверка числа на простоту. Дано натуральное число n>1. Проверьте, является ли оно простым.");
+    System.out.println("7)  Разложение на множители. Дано натуральное число n>1. Выведите все простые множители этого числа в порядке неубывания с учетом кратности.");
+    System.out.print("  Введи число N: ");
+    N = scanner.nextInt();
+    System.out.print("  Результат: ");
+    task_7(N, 2);
+
+    // 8)
+    System.out.println("\n8) Палиндром. Дано слово, состоящее только из строчных латинских букв. Проверьте, является ли это слово палиндромом.");
+    System.out.print("  Введи слово: ");
+    String line = scanner.next();
+    System.out.println("  Результат: " + task_8(line));
+
+    // 9)
+    System.out.println("9) Без двух нулей. Даны числа a и b. Определите, сколько существует последовательностей из a нулей и b единиц, в которых никакие два нуля не стоят рядом.");
+    System.out.print("  Введи число a: ");
+    int a = scanner.nextInt();
+    System.out.print("  Введи число b: ");
+    int b = scanner.nextInt();
+    System.out.print("  Кол-во последовательностей: " + task_9(a, b));
+
+    // 10)
+    System.out.println("\n10) Разворот числа. Дано число n, десятичная запись которого не содержит нулей. Получите число, записанное теми же цифрами, но в противоположном порядке.");
+    System.out.print("  Введи число: ");
+    N = scanner.nextInt();
+    System.out.println("  Результат: " + task__10(N, 0));
+
+    // 11)
+    System.out.println("11) Количество единиц. Дана последовательность натуральных чисел (одно число в строке), завершающаяся двумя числами 0 подряд. Определите, сколько раз в этой последовательности встречается число 1.");
+    System.out.print("  Введи числа: ");
+    System.out.print("  Результат: " + task_11());
+
+    // 12)
+    System.out.println( "\n12) Вывести нечетные числа последовательности. Дана последовательность натуральных чисел (одно число в строке), завершающаяся числом 0. Выведите все нечетные числа из этой последовательности, сохраняя их порядок.");
+    System.out.print("  Введи числа: ");
+    System.out.println("  Результат: ");
+ 
+    task_12();
 
     scanner.close();
   }
